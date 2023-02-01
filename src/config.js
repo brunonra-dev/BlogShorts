2e2370c684ebeeb710c9122f9c7016a3b626c514
brunonra-dev <40237383+brunonra-dev@users.noreply.github.com>
2023-02-01 19:03:59 -0300

    add head component & Site config file

---
+++ src/config.js
@@ -0,0 +1,18 @@
+export const SITE = {
+  name: 'BlogShort',
+  title: 'Astro - BlogShort',
+  description: 'Personal blog theme for Astro',
+  lang: 'en',
+  //TODO: add your own url
+  url: 'https://astro-ink.vercel.app',
+  githubUrl: 'https://github.com/one-aalam/astro-ink',
+  listDrafts: true,
+  image: 'https://raw.githubusercontent.com/one-aalam/astro-ink/main/public/astro-banner.png',
+  // Optional, user/author settings (example)
+  // Author: name
+  author: '', // Example: Fred K. Schott
+  // Author: Twitter handler
+  authorTwitter: '', // Example: FredKSchott
+  // Author: Image external source
+  authorImage: '', // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg
+}
